d7ceb3ca6fd5cc5255f32b96477a71b5ae715bda
Juan-cruz22 <ghigliani02@gmail.com>
2025-02-20 18:49:48 -0300

    Aplicación de Less en Card

--- src/components/PizzaCard/pizzaCard.jsx
+++ src/components/PizzaCard/pizzaCard.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import style from './pizzaCard.module.css';
+import style from'./pizzaCard.module.less';
 
 const PizzaCard = ({ name, ingredients, price, detail, photo }) => {
   return (
     <div className={style.containerCard}>
-      {         <button className={style.BotonCarrito}>Agregar a mi pedido por ${price}</button>}
+      <button className={style.BotonCarrito}>Agregar a mi pedido por ${price}</button>
       <div className={style.content}>
         <img src={photo} alt={name} className={style.photo} />
         <div className={style.textContainer}>
           <h3>{name}</h3>
-          {/* Aquí iteramos sobre el array de ingredientes */}
           {Array.isArray(ingredients) ? (
             ingredients.map((ingredient, index) => (
               <p key={index} className="ingredients">{ingredient}</p>
             ))
           ) : (
-            <p className="ingredients">{ingredients}</p> // Si no es un array (en el caso de Caprese), se muestra como un único string.
+            <p className="ingredients">{ingredients}</p>
           )}
           <p className={style.detail}>{detail}</p>
         </div>
