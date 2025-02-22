1f68c781aab75220f63ba055f179840816570edd
Juan-cruz22 <ghigliani02@gmail.com>
2025-02-22 15:08:48 -0300

    status

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/FratelliDiCuore-VirtualMenu/',  // Añadir esta línea
+  base: '/FratelliDiCuore-VirtualMenu/',  // Asegúrate de que esté configurado correctamente
 });
