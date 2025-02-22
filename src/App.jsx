a0dc45180416a059fb891fc465d331201b6c42f5
Juan-cruz22 <ghigliani02@gmail.com>
2025-02-22 15:17:49 -0300

    Corregir importaciones en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import PizzaCards from '../src/components/PizzaCards/pizzaCards';
+import PizzaCards from './components/PizzaCards/pizzaCards';
 import Head from './components/Head/head';
 import ButtonCarrito from './components/ButtonCarrito/buttonCarrito';
 
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/FratelliDiCuore-VirtualMenu/',  // Asegúrate de que esté configurado correctamente
+  base: '/FratelliDiCuore-VirtualMenu/',  // Esta línea está configurada correctamente
 });
