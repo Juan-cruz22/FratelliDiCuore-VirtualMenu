326bd1c474cec6e9c7a4bcb354a651d31463a796
Juan-cruz22 <ghigliani02@gmail.com>
2025-02-26 19:55:06 -0300

    Agregado del 40% de los articulos

--- src/Data.jsx
+++ src/Data.jsx
@@ -3,6 +3,41 @@ import Caprese from "../src/Photos/Caprese.png";
 
 export default [
     {
+        name:'Marinara',
+        ingredients: ["Salsa de tomate", "Oregano", "Ajo", "Albahaca"],
+        price:'5.300',
+        detail:'Muy rica',
+        photo: Margherita,
+    },
+    {
+        name:'Muzza',
+        ingredients: ["Salsa de tomate", "Mozzarella", "Oregano"],
+        price:'7.500',
+        detail:'Muy rica',
+        photo: Margherita,
+    },
+    {
+        name:'Caramelita',
+        ingredients: ["Aceite de oliva", "Mozzarella", "Cebolla caramelizada", "Romero"],
+        price:'7.800',
+        detail:'Muy rica',
+        photo: Margherita,
+    },
+    {
+        name:'Margherita',
+        ingredients: ["Salsa de tomate", "Mozzarella", "Albahaca"],
+        price:'6.500',
+        detail:'Muy rica',
+        photo: Margherita,
+    },
+    {
+        name:'Caprese',
+        ingredients: ['Salsa de tomate', 'Mozzarella', 'albahaca', 'tomate cherry'],
+        price:'6.800',
+        detail:'Muy rica',
+        photo: Caprese,
+    },
+    {
         name:'Margherita',
         ingredients: ["Salsa de tomate", "Mozzarella", "Albahaca"],
         price:'6.500',
