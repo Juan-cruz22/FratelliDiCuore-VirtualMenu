d4758c4094877cb79af4e3637dba6cd126eeb46b
Juan-cruz22 <ghigliani02@gmail.com>
2025-02-27 15:12:50 -0300

    Accion buttonPedido retoceso

--- src/components/DetailPizza/detail.jsx
+++ src/components/DetailPizza/detail.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import style from "./detail.module.less";
-
 import pizzaData from "../../Data";
+import { useNavigate } from "react-router-dom";
 
 const Detail = () => {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/`);
+  }
+
+
   const { name } = useParams();
-
   const pizza = pizzaData.find(pizza => pizza.name === name);
-
   if (!pizza) {
     return <div>Pizza no encontrada</div>; 
   }
 
   return (
     <div>
+    <button className={style.buttonRetroceso}>{"<-"}</button>
     <img className={style.photograp} src={pizza.photo} alt={pizza.name} />
       <h1 className={style.nombre}>{pizza.name}</h1>
       <p className={style.detalle}>{pizza.detail}</p>
@@ -26,7 +33,7 @@ const Detail = () => {
           <div>
             <button className={style.buttonBR}>Borde relleno</button>
           <div> 
-          <button className={style.buttonPedido}>Agregar a mi pedido (${pizza.price})</button>
+          <button className={style.buttonPedido} onClick={handleClick}>Agregar a mi pedido (${pizza.price})</button>
           </div> 
           </div>
       </div>
