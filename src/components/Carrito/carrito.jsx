ce9ebcc715ec27bb264acd8f7bae69d63caa0781
Juan-cruz22 <ghigliani02@gmail.com>
2025-02-27 15:59:05 -0300

    Inicio maquetación Carrito

--- src/components/Carrito/carrito.jsx
+++ src/components/Carrito/carrito.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom"; // Importa useLocation
+import style from "./carrito.module.less";
+import header from "../headCarrito.png";
 
 const Carrito = () => {
-    return(
-        <div>
-            Aca va el carrito
+  const location = useLocation(); // Obtén la ubicación actual
+
+  // Cambiar el color de fondo cuando la ruta es "/carrito"
+  useEffect(() => {
+    if (location.pathname === "/Carrito") {
+      document.body.style.backgroundColor = "blue"; // Cambia el color del fondo del body
+    }
+
+    // Limpiar el estilo cuando el componente se desmonte o cambie la ruta
+    return () => {
+      document.body.style.backgroundColor = ""; // Vuelve al color original
+    };
+  }, [location.pathname]); // Ejecuta el efecto cada vez que la ruta cambia
+
+  return (
+    <div>
+      <img className={style.imgHead} src={header} alt="Header Carrito" />
+        <div className={style.pedido}>
+            <h1 className={style.titulo}>
+              mi pedido
+            </h1>
+            <div className={style.buttonCompletar}>
+                <button className={style.buttonRealizar}>Realizar pedido</button>
+            </div>
         </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Carrito;
--- src/components/Head/head.jsx
+++ src/components/Head/head.jsx
@@ -3,10 +3,13 @@ import head from "../Head.png";
 import style from "../Head/head.module.css"
 
 const Head = () => {
+
     return(
+    <div className={style.back}>
         <div className={style.divhead}>
             <img className={style.image} src={head}></img>
         </div>
+    </div>
     )
 }
 
