b74780a63112040dec02aa87768d4af8fce93092
Juan-cruz22 <ghigliani02@gmail.com>
2025-02-27 15:25:28 -0300

    Llamado a la acciòn de retroceder en el button del Detail

--- src/components/DetailPizza/detail.jsx
+++ src/components/DetailPizza/detail.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import style from "./detail.module.less";
 import pizzaData from "../../Data";
-import { useNavigate } from "react-router-dom";
 
 const Detail = () => {
 
   const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handleBackClick = () => {
     navigate(`/`);
   }
-
 
   const { name } = useParams();
   const pizza = pizzaData.find(pizza => pizza.name === name);
@@ -21,22 +20,22 @@ const Detail = () => {
 
   return (
     <div>
-    <button className={style.buttonRetroceso}>{"<-"}</button>
-    <img className={style.photograp} src={pizza.photo} alt={pizza.name} />
+      <div className={style.photoContainer}>
+        <img className={style.photograp} src={pizza.photo} alt={pizza.name} />
+        <button className={style.buttonRetroceso} onClick={handleBackClick}>Volver</button>
+      </div>
       <h1 className={style.nombre}>{pizza.name}</h1>
       <p className={style.detalle}>{pizza.detail}</p>
       <div className={style.contPrice}>
-      <p className={style.precio}>${pizza.price}</p>
+        <p className={style.precio}>${pizza.price}</p>
         <p className={style.extras}>EXTRAS ($2.500)</p>
-            <button className={style.buttonAlb}>Pesto de Albahaca</button>
-            <button className={style.buttonDia}>Salsa Diavola</button>
-          <div>
-            <button className={style.buttonBR}>Borde relleno</button>
-          <div> 
-          <button className={style.buttonPedido} onClick={handleClick}>Agregar a mi pedido (${pizza.price})</button>
-          </div> 
-          </div>
-      </div>
+        <button className={style.buttonAlb}>Pesto de Albahaca</button>
+        <button className={style.buttonDia}>Salsa Diavola</button>
+        <div>
+          <button className={style.buttonBR}>Borde relleno</button>
+        </div>
+        <button className={style.buttonPedido} onClick={handleBackClick}>Agregar a mi pedido (${pizza.price})</button>
+      </div> 
     </div>
   );
 }
